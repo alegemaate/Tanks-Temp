ad475b0912732f48ac702dd65b91a344c49645f4
Allan Legemaate <alegemaate@gmail.com>
2021-11-03 21:11:52 -0400

    Add helper to tile map

--- src/game/tile-map.h
+++ src/game/tile-map.h
@@ -7,18 +7,32 @@
 #include "../util/coordinate.h"
 #include "barrier.h"
 
+struct TileMapNeighbours {
+  int n;
+  int ne;
+  int e;
+  int se;
+  int s;
+  int sw;
+  int w;
+  int nw;
+};
+
 class TileMap {
  public:
   void generateMap(unsigned char width, unsigned char height);
 
  private:
+  TileMapNeighbours getNeighbours(const unsigned char x,
+                                  const unsigned char y) const;
+
   unsigned char width;
   unsigned char height;
 
   std::vector<Barrier> barriers;
-  std::vector<Coordinate> startLocations;
+  std::vector<Coordinate> start_locations;
 
-  int map_temp[255][255];
+  std::vector<std::vector<int>> tile_map;
 };
 
 #endif  // SRC_GAME_TILE_MAP_H_
